c4ebed1b842f07a12597d4a8f256c4cce1dac360
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2022-11-01 19:30:42 +0100

    Project initialization

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="">
+        <div className="MainSection">
+          <div className="content">
+            <div className="ProfileSection"></div>
+            <div className="LinkSection"></div>
+          </div>
+        </div>
+        <div className="SocialSection"></div>
+      </div>
+      <div className="FooterSection">
+        <div className="container">
+          <div className="footerContent"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
