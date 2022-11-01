800b1711585e2b5eee7664514076a9864ec61e48
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2022-11-01 21:02:23 +0100

    wip: main section

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import './App.css';
+import './App.scss';
+import ProfileImg from './profile-pic.jpg';
+import ShareIcon from './share-icon.svg';
 
 function App() {
   return (
     <div className="App">
-      <div className="">
-        <div className="MainSection">
+      <div className="main-wrapper">
+        <div className="main-section">
           <div className="content">
-            <div className="ProfileSection"></div>
-            <div className="LinkSection"></div>
+            <div className="profile-section">
+              <img src={ProfileImg} id="profile__img" alt="Oluwakemi" />
+              <p className="twitter-id" id="twitter">innykemi</p>
+              <p className="slack-id" id="slack">InnyKemi</p>
+              <span className="share-button">
+                <img src={ShareIcon} alt="share-icon" />
+              </span>
+            </div>
+            <div className="link-section">
+              <span className="link">Twitter Link</span>
+              <span className="link"></span>
+              <span className="link"></span>
+              <span className="link"></span>
+              <span className="link"></span>
+              <span className="link"></span>
+            </div>
           </div>
         </div>
-        <div className="SocialSection"></div>
+        <div className="social-section"></div>
       </div>
-      <div className="FooterSection">
+      <div className="footer-section">
         <div className="container">
-          <div className="footerContent"></div>
+          <div className="footer-content"></div>
         </div>
       </div>
     </div>
